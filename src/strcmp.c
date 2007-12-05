bc378ab3fdd39cfff1e5d4ce3dea52c16afcd5ac
markos <devnull@localhost>
2007-12-05 19:07:17 +0200

    [svn r43] initial commit to strcmp

--- src/strcmp.c
+++ src/strcmp.c
@@ -33,29 +33,32 @@ int vec_strcmp(const uint8_t *src1pp, const uint8_t *src2pp) {
   READ_PREFETCH_START1(src1);
   READ_PREFETCH_START2(src2);
 
-  STRCMP_UNTIL_SRC1_WORD_ALIGNED(src1, src2);
+  uint32_t src1al = (uint32_t)src1 % sizeof(uint32_t);
+  if (src1al)
+    STRCMP_UNTIL_SRC1_WORD_ALIGNED(src1, src2, src1al);
 
   // Take the word-aligned long pointers of src2 and dest.
-  uint8_t src2offset4 = ((uint32_t)(src2) & (sizeof(uint32_t)-1));
-  uint32_t *src1l = (uint32_t *)(src1);
-  const uint32_t *src2l = (uint32_t *)(src2 -src2offset4);
+  uint8_t src2al = ((uint32_t)(src2) % sizeof(uint32_t));
 
-  STRCMP_UNTIL_SRC1_IS_ALTIVEC_ALIGNED(src1, src1l, src2, src2l, src2offset4);
+  uint32_t *src1l = (uint32_t *)(src1);
+  const uint32_t *src2l = (uint32_t *)(src2 -src2al);
+
+  src1al = (uint32_t)src1 % ALTIVECWORD_SIZE;
+  if (src1al)
+    STRCMP_UNTIL_SRC1_IS_ALTIVEC_ALIGNED(src1, src1l, src2, src2l, src1al, src2al);
 
   // Now src1 is word aligned. If possible (ie if there are enough bytes left)
   // we want to align it to 16-byte boundaries as well.
   // For this we have to know the word-alignment of src2 also.
 
-  src2 = (uint8_t *) src2l +src2offset4;
-
   if (((uint32_t)(src2) % ALTIVECWORD_SIZE) == 0) {
     // Now, both buffers are 16-byte aligned, just copy everything directly
-    STRCMP_LOOP_SINGLE_ALTIVEC_WORD_ALIGNED(src1, src1l, src2, src2l, src2offset4);
-    src2l = (uint32_t *)(src2 -src2offset4);
+    STRCMP_LOOP_SINGLE_ALTIVEC_WORD_ALIGNED(src1, src1l, src2, src2l, src2al);
+    src2l = (uint32_t *)(src2 -src2al);
   } else {
     // src2 is not 16-byte aligned so we have to a little trick with Altivec.
-    STRCMP_LOOP_SINGLE_ALTIVEC_WORD_UNALIGNED(src1, src1l, src2, src2l, src2offset4);
-    src2l = (uint32_t *)(src2 -src2offset4);
+    STRCMP_LOOP_SINGLE_ALTIVEC_WORD_UNALIGNED(src1, src1l, src2, src2l, src2al);
+    src2l = (uint32_t *)(src2 -src2al);
   }
 
   return 0;
