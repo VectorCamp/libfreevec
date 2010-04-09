0100b81b6a7744ee09a0be5d5d6b1ff2648852b5
Konstantinos Margaritis <markos@codex.gr>
2010-04-09 16:49:06 +0300

    minor fix

--- src/string/arch/altivec/memcpy.h
+++ src/string/arch/altivec/memcpy.h
@@ -27,10 +27,6 @@
 
 /* $Id$ */
 
-#include <stdint.h>
-#include <stddef.h>
-
-#include "common.h"
 #include "arch/altivec.h"
 
 static inline void copy_fwd_rest_blocks_aligned(word_t *d, const word_t *s, size_t blocks) {
